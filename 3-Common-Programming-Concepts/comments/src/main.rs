f6b8cc26207a63b12d42804c5feb20d6c3297329
MrOrdenador <srzaixgd@gmail.com>
2025-12-27 11:37:35 +0100

    Adding the comments section of the third part

---
+++ 3-Common-Programming-Concepts/comments/src/main.rs
@@ -0,0 +1,18 @@
+fn main() {
+    // dawg I've been using these since the first part of the book 😭
+
+    // hello, world
+
+    // Basically
+    // Putting
+    // a lot of
+    // Comments
+    // (instead of /* */, why isn't this in the book??!?!?)
+
+    let lucky_number = 7; // I'm feeling lucky today
+    
+    // or
+
+    // I'm feeling lucky today
+    let lucky_number = 7; 
+}
